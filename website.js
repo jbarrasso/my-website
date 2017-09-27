c2d47e3e1a6d597b985cb386e5863d62759094c5
jbarrasso <JeffBarrasso@Jeffreys-MacBook-Pro-2.local>
2017-09-26 19:55:45 -0700

    Update website.js listener function

--- website.js
+++ website.js
@@ -3,6 +3,8 @@ var app = express();
 
 var router = express.Router();
 var path = require('path')
+
+var port = process.env.PORT || 5000;
  
 app.use(express.static(path.join(__dirname, 'views/public')));  
 
@@ -24,7 +26,10 @@ router.get('/',function(req, res){
 app.use('*',function(req, res){
   res.send('Error 404: Not Found!');
 });
-  
+
+app.listen(port);                                  
+console.log('$APP_NAME is listening on port ' + port);
+ /* 
 app.listen(3000,function(){
   console.log('Server running at Port 3000');
-});
+});*/
